4d8ca3796220e7523682fd79268ce4738b6df3da
Ryan Amer <ramer2@jh.edu>
2024-09-14 00:23:42 -0400

    Started on helper functions, implemented mirror_h

--- Assignment 2/csf_assign02/c_imgproc_fns.c
+++ Assignment 2/csf_assign02/c_imgproc_fns.c
@@ -5,6 +5,44 @@
 #include "imgproc.h"
 
 // TODO: define your helper functions here
+int all_tiles_nonempty( int width, int height, int n );
+int determine_tile_w( int width, int n, int tile_col );
+int determine_tile_x_offset( int width, int n, int tile_col );
+int determine_tile_h( int height, int n, int tile_row );
+int determine_tile_y_offset( int height, int n, int tile_row );
+void copy_tile( struct Image *out_img, struct Image *img, int tile_row, int tile_col, int n );
+
+uint32_t get_r(uint32_t pixel) { //shift 
+    return (pixel >> 24) & 0xFF;
+}
+
+uint32_t get_g(uint32_t pixel) {
+    return (pixel >> 16) & 0xFF;
+}
+
+uint32_t get_b(uint32_t pixel) {
+    return (pixel >> 8) & 0xFF;
+}
+
+uint32_t get_a(uint32_t pixel) {
+    return pixel & 0xFF;
+}
+
+uint32_t make_pixel(uint32_t r, uint32_t g, uint32_t b, uint32_t a) {
+    return (r << 24) | (g << 16) | (b << 8) | a;
+}
+
+uint32_t get_pixel(struct Image *img, int32_t x, int32_t y) {
+    return img->data[y * img->width + x];
+}
+
+void set_pixel(struct Image *img, int32_t x, int32_t y, uint32_t pixel) {
+    img->data[y * img->width + x] = pixel;
+}
+
+uint32_t to_grayscale( uint32_t pixel );
+uint32_t blend_components( uint32_t fg, uint32_t bg, uint32_t alpha );
+uint32_t blend_colors( uint32_t fg, uint32_t bg );
 
 // Mirror input image horizontally.
 // This transformation always succeeds.
@@ -14,7 +52,19 @@
 //   output_img - pointer to the output Image (in which the transformed
 //                pixels should be stored)
 void imgproc_mirror_h( struct Image *input_img, struct Image *output_img ) {
-  // TODO: implement
+  
+  //iterate over each row, swap the pixels to produce mirror image
+  for(int32_t y = 0; y < input_img->height; y++) {
+    for(int32_t x = 0; x < input_img->width; x++) {
+
+      int32_t mirrored_index = input_img->width - 1 - x; //tile to swap with
+
+      // Swap current pixel to mirrored position in output image
+      uint32_t pixel = get_pixel(input_img, x, y);
+      set_pixel(output_img, mirrored_index, y, pixel);
+    }
+  }
+
 }
 
 // Mirror input image vertically.
