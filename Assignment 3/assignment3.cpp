4be19bb49e3d297d90728afe960b30039d41c879
alpyd <aedemirtas04@gmail.com>
2024-10-10 17:56:37 -0400

    Wrote out skeleton for processing arguments

--- Assignment 3/assignment3.cpp
+++ Assignment 3/assignment3.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 
 int main(int argc, char *argv[]) {
     // argc: number of command-line arguments
@@ -6,10 +7,27 @@ int main(int argc, char *argv[]) {
 
     std::cout << "Number of arguments: " << argc << std::endl;
 
-    // print out arguments
-    for (int i = 0; i < argc; ++i) {
-        std::cout << "Argument " << i << ": " << argv[i] << std::endl;
+    int numSets = std::stoi(argv[1]);
+    int numBlocks = std::stoi(argv[2]);
+    int blockMem = std::stoi(argv[3]);
+    bool writeAllocate = (argv[4] == "write-allocate");
+    bool writeThrough = (argv[5] == "write-through");
+
+    std::string inputLine;
+    while(std::getline(std::cin, inputLine)){
+        std::istringstream commandPrompt = std::istringstream(inputLine);
+        char command;
+        uint32_t memoryAddress;
+
+        commandPrompt >> command;
+        commandPrompt >> memoryAddress;
+
+        // Execute Cache Action
+        
     }
+
+
+    // Print Out Cache Arguments
 
     return 0;
 }
