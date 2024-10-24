01b628f9ddf814c80c10677b32072eb85081842b
alpyd <aedemirtas04@gmail.com>
2024-10-23 22:59:31 -0400

    Submitted assignment

--- Assignment 3/assignment3.cpp
+++ Assignment 3/assignment3.cpp
@@ -3,36 +3,14 @@
 #include "cachesimulator.h"
 #include <cstring>
 
+// Ensure that the inputted number is a valid positive power of two
 bool validPowerOfTwo(int num){
     return num > 0 && (num & (num - 1)) == 0;
 }
 
-bool parametersValid(int argc, char* argv[]) {
-    if (argc != 7) {
-        std::cerr << "Error - You must input 6 parameters" << std::endl;
-        return false;
-    }
 
-    for (int i = 1; i <= 3; i++) {
-        int value;
-        try {
-            value = std::stoi(argv[i]);
-        } catch (const std::invalid_argument& e) {
-            std::cerr << "Error - Invalid number format for argument " << i << std::endl;
-            return false;
-        }
-
-        if (value <= 0 || !validPowerOfTwo(value)) {
-            std::cerr << "Error - Argument " << i << " must be a positive power of two" << std::endl;
-            return false;
-        }
-    }
-
-    if (std::stoi(argv[3]) < 4) {
-        std::cerr << "Error - Block size must be at least 4 bytes" << std::endl;
-        return false;
-    }
-
+bool stringInputsValid(char* argv[]){
+    // Ensure that either write-allocate or no-write-allocate parameter is provided and throw an error if neither provided
     bool isWriteAllocate = false;
     if (strcmp(argv[4], "write-allocate") == 0) {
         isWriteAllocate = true;
@@ -43,6 +21,7 @@ bool parametersValid(int argc, char* argv[]) {
         return false;
     }
 
+    // Ensure that either write-through or write-back parameter is provided and throw an error if neither provided
     bool isWriteThrough = false;
     if (strcmp(argv[5], "write-through") == 0) {
         isWriteThrough = true;
@@ -53,13 +32,59 @@ bool parametersValid(int argc, char* argv[]) {
         return false;
     }
 
+    // Ensure that the cache combination of no-write-allocate with write-back is specified. Wouldn't write cache to store
     if (!isWriteThrough && !isWriteAllocate) {
         std::cerr << "Error - Does not make sense to combine no-write-allocate with write-back." << std::endl;
         return false;
     }
 
+    // Ensure that the provided eviciton policy is either fifo or lru
     if (strcmp(argv[6], "fifo") != 0 && strcmp(argv[6], "lru") != 0) {
         std::cerr << "Error - Eviction policy must either be fifo or lru" << std::endl;
+        return false;
+    }
+
+    return true;
+}
+
+bool numbersValid(char* argv[]) {
+    // Ensure that the numerical inputs are correct
+    for (int i = 1; i <= 3; i++) {
+
+        // Try to convert the inputted value to a number and throw an error if unsuccessful
+        int value;
+        try {
+            value = std::stoi(argv[i]);
+        } catch (const std::invalid_argument& e) {
+            std::cerr << "Error - Invalid number format for argument " << i << std::endl;
+            return false;
+        }
+
+        // Throw an error if the number is not a valid power of two
+        if (value <= 0 || !validPowerOfTwo(value)) {
+            std::cerr << "Error - Argument " << i << " must be a positive power of two" << std::endl;
+            return false;
+        }
+    }
+
+    // Specifically make sure that the block size is at least 4 bytes
+    if (std::stoi(argv[3]) < 4) {
+        std::cerr << "Error - Block size must be at least 4 bytes" << std::endl;
+        return false;
+    }
+
+    return true;
+}
+
+bool parametersValid(int argc, char* argv[]) {
+    // Check if user has 6 parameters
+    if (argc != 7) {
+        std::cerr << "Error - You must input 6 parameters" << std::endl;
+        return false;
+    }
+
+    // Ensure that the number and string parameters are both valid
+    if (!numbersValid(argv) && !stringInputsValid(argv)){
         return false;
     }
 
@@ -70,12 +95,15 @@ int main(int argc, char *argv[]) {
     // argc: number of command-line arguments
     // argv: array of C-style strings (char*), representing each argument
 
+    // Ensure that the provided parameters are valid. Otherwise, return null
     if (!parametersValid(argc, argv)) {
         return 1;
     }
 
+    // Print out the number of arguments
     std::cout << "Number of arguments: " << argc << std::endl;
 
+    // Initialize the user parameters
     int numSets = std::stoi(argv[1]);
     int numBlocks = std::stoi(argv[2]);
     int blockMem = std::stoi(argv[3]);
@@ -83,8 +111,10 @@ int main(int argc, char *argv[]) {
     bool writeThrough = (strcmp(argv[5], "write-through") == 0);
     bool evictionLRU = (strcmp(argv[6], "lru") == 0);
 
+    // Instantiate the CacheSimulator class
     CacheSimulator csim(numSets, numBlocks, blockMem, writeAllocate, writeThrough, evictionLRU);
 
+    // Read in the commands using std::cin to get the line and istringstream to parse the command
     std::string inputLine;
     while (std::getline(std::cin, inputLine)) {
         std::istringstream commandPrompt = std::istringstream(inputLine);
@@ -92,15 +122,18 @@ int main(int argc, char *argv[]) {
         std::string memoryAddressStr;
         uint32_t memoryAddress;
 
-        // Read the command ('l' or 's')
+        // Read the command ('l' or 's') and return error if otherwise
         if (!(commandPrompt >> command) || (command != 'l' && command != 's')) {
             std::cerr << "Error: Invalid command format." << std::endl;
+            // Return 1 for error
             return 1;
         }
 
-        // Read the memory address as a hexadecimal string
+        // Read the memory address as a hexadecimal string and return error if unsuccessful
         if (!(commandPrompt >> memoryAddressStr)) {
             std::cerr << "Error: Invalid memory address format." << std::endl;
+
+            // Return 1 for error
             return 1;
         }
 
@@ -111,6 +144,9 @@ int main(int argc, char *argv[]) {
         csim.executeCommand(command, memoryAddress);
     }
 
+    // Print the summary info once all commands have been executed
     csim.printSummaryInfo();
+
+    // Return 0 for success
     return 0;
 }
--- Assignment 3/cachesimulator.cpp
+++ Assignment 3/cachesimulator.cpp
@@ -3,46 +3,35 @@
 #include <iostream>
 #include <cmath>
 
-// Constructor for the CacheSimulator takes in the user-inputted parameters and initializes the tracker variables to 0
 CacheSimulator::CacheSimulator(int numOfSets, int setSize, int blockSize, bool writeAllocate, bool writeThrough, bool evictionLRU)
-    : numOfSets(numOfSets), setSize(setSize), blockSize(blockSize), writeAllocate(writeAllocate), writeThrough(writeThrough), evictionLRU(evictionLRU),
-    totLoads(0), totStores(0), totCycles(0), LTotHits(0), LTotMisses(0), STotHits(0), STotMisses(0), timestamp(0)  {
+    : numOfSets(numOfSets), setSize(setSize), blockSize(blockSize), 
+      writeAllocate(writeAllocate), writeThrough(writeThrough), evictionLRU(evictionLRU), 
+      totCycles(0), STotMisses(0), timestamp(0) {
 
-    // Populate an empty cache
     for(int i = 0; i < numOfSets; i++){
-        // Create a new Set based on the number of user-specified Sets
         Set newSet;
         for (int j = 0; j < setSize; j++){
-            // Create a new Slot based on the number of user-specified Slots
             Slot newSlot;
-
-            // Set the valid and dirty parameters to false and add the newSlot
             newSlot.valid = false;
             newSlot.dirty = false;
+            newSlot.access_ts = 0; // Initialize access_ts
+            newSlot.load_ts = 0;   // Initialize load_ts
             newSet.slots.push_back(newSlot);
         }
         cache.sets.push_back(newSet);
     }
 }
 
-
 void CacheSimulator::executeCommand(char command, uint32_t memoryAddress){
-    // If the command is 's' for store, call a store
     if(command == 's') {
-        // Increase the total store calls
         totStores++;
-
-        //Track number of store hits/misses and call store method
-        if(store(memoryAddress)){ 
+        if(store(memoryAddress)){ //track of store hits/misses
             STotHits++;
         } else {
             STotMisses++;
         }
-    } else if(command == 'l'){ 
-        // If the command is 'l' for load, call a load and increase the total load calls
+    } else if(command == 'l'){ //track of load hits/misses
         totLoads++;
-
-        // Track of load hits/misses
         if(load(memoryAddress, false)){
             LTotHits++;
         } else {
@@ -51,171 +40,123 @@ void CacheSimulator::executeCommand(char command, uint32_t memoryAddress){
     } 
 }
 
-uint32_t CacheSimulator::readTag(uint32_t memoryAddress){
-    // Take the natural log of the offset and index sizes to get the number of bits
-    int numBitsOffset = std::log(blockSize)/std::log(2);
-    int numBitsIndex = std::log(numOfSets)/std::log(2);
+uint32_t CacheSimulator::readTag(uint32_t memoryAddress) {
+    int numBitsOffset = std::log2(blockSize);  // Number of bits for the block offset
+    int numBitsIndex = std::log2(numOfSets);   // Number of bits for the index
 
-    // Right shift the memory address by the number of total offset and index bits to get the tap
     return memoryAddress >> (numBitsOffset + numBitsIndex);
 }
 
-uint32_t CacheSimulator::readIndex(uint32_t memoryAddress){
-    // Take the natural log of the offset and index sizes to get the number of bits
-    int numBitsOffset = std::log(blockSize)/std::log(2);
-    int numBitsIndex = std::log(numOfSets)/std::log(2);
+uint32_t CacheSimulator::readIndex(uint32_t memoryAddress) {
+    int numBitsOffset = std::log2(blockSize);  // Number of bits for the block offset
+    int numBitsIndex = std::log2(numOfSets);   // Number of bits for the index
 
-    // Right shift by the number of offset bits and then isolate the number of index bits using &
     return (memoryAddress >> numBitsOffset) & ((1 << numBitsIndex) - 1);
 }
 
-bool CacheSimulator::load(uint32_t memoryAddress, bool isDirty){
-    // Read the tag and index from the memory index
-    uint32_t tag = readTag(memoryAddress);
-    uint32_t index = readTag(memoryAddress);
-
-    // Locate the desired set from the index
-    Set &set = cache.sets[index];
-    
-    // Iterate over the Slots in a set
-    for (unsigned int i = 0; i < set.slots.size(); i++){ 
-        // Get the slot at a specific index
-        Slot &slot = set.slots.at(i);
-
-        // If the solid is found to match, success! Load.
-        if(slot.valid && slot.tag == tag){    
-            // Set the access timestamp to current timestamp 
-            slot.access_ts = ++timestamp;
-
-            // Carry the data if the slot is dirty
-            if(isDirty){
-                slot.dirty = true;
-            }
-
-            // 1 cycle per load operation
-            totCycles++; 
-            return true;
-        }
-    }
-
-    // If could not hit the load, handle the load miss
-    handleLoadMiss(set, tag, isDirty);
-
-    // Return false, indicating that there was a miss
-    return false;
-    
-
-}
-
-void CacheSimulator::handleLoadMiss(Set& set, uint32_t tag, bool isDirty){
-    // Choose the slot to load into
-    Slot chosenSlot = set.slots.at(chooseSlotIndex(set));
-
-    // If FIFO policy, update the timestamp
-    if(!evictionLRU){
-        chosenSlot.load_ts = ++timestamp;
-    }
-
-    if(chosenSlot.valid && chosenSlot.dirty){ 
-        // Add 100 cycles per 4 byte quantity
-        totCycles += ((blockSize / 4)  * 100);
-    }
-
-    // Update the Slot with the provided parameters from the execution and memory Address
-    chosenSlot.tag = tag;
-    chosenSlot.valid = true;
-    chosenSlot.dirty = isDirty;
-    chosenSlot.access_ts = ++timestamp;
-
-    // Add 100 cycles per 4 byte quantity
-    totCycles += ((blockSize / 4) * 100); 
-}
 
 uint32_t CacheSimulator::chooseSlotIndex(Set& set){
-    // First see if there are any invalid or uninitialized slots
-    // Return that slot index if it is
-    for(unsigned int i = 0; i < set.slots.size(); i++){
+    for(uint32_t i = 0; i < set.slots.size(); i++){
         if(!set.slots.at(i).valid){
             return i;
         }
     }
 
-    // Otherwise, use the eviction policy to select the slot to be evicted
     int chosenIndex = 0;
     if(evictionLRU) {
-        // If eviction is LRU, choose the slot that was accessed least recently (linear search)
-        for(unsigned int i = 0; i < set.slots.size(); i++){
+        for(uint32_t i = 0; i < set.slots.size(); i++){
             if(set.slots.at(i).access_ts < set.slots.at(chosenIndex).access_ts){
                 chosenIndex = i;
             }
         }
     } else {
-        // If eviction is FIFO, choose the slot that was loaded least recently (linear search)
-        for(unsigned int i = 0; i < set.slots.size(); i++){
+        for(uint32_t i = 0; i < set.slots.size(); i++){
             if(set.slots.at(i).load_ts < set.slots.at(chosenIndex).load_ts) {
                 chosenIndex = i;
             }
         }
     }
 
-    // Return the chosen index once found under these parameters
     return chosenIndex;
 }
 
-bool CacheSimulator::store(uint32_t memoryAddress){
-    // Read tag and index from memory address
+bool CacheSimulator::load(uint32_t memoryAddress, bool isDirty){ //reading from cache
     uint32_t tag = readTag(memoryAddress);
-    uint32_t index = readTag(memoryAddress);
-
-    // Locate the desired Set using the provided index
+    uint32_t index = readIndex(memoryAddress);
     Set &set = cache.sets[index];
-
-    for (unsigned int i = 0; i < set.slots.size(); i++){
-        // Specify a slot of a certain index in the Set
-        Slot slot = set.slots.at(i);
-        if(slot.valid && slot.tag == tag){
-            // Success! We found tag, let's store now.
-            timestamp++;
-
-            // Update access timestamp for LRU eviction
-            slot.access_ts = timestamp;
-
-            // Handle write-through and write-back instances
-            if(writeThrough){
-                load(memoryAddress, false);
-                // Handle cycles
-                totCycles += 100; 
-            } else {
-                load(memoryAddress, true);
+    for (unsigned int i = 0; i < set.slots.size(); i++){ //iterate over blocks in set
+        Slot &slot = set.slots.at(i);
+        if(slot.valid && slot.tag == tag){    
+            
+            slot.access_ts = ++timestamp;
+            if(isDirty){
+                slot.dirty = true;
             }
-
-            // Increment cycles and return true for successful store
-            totCycles++;
+            totCycles++; //1 cycle per load operation
             return true;
         }
     }
 
-    // If not able to find the tag, handle the store miss
-    handleStoreMiss(memoryAddress);
+    handleLoadMiss(set, tag, isDirty);
     return false;
-
+    
 }
 
-void CacheSimulator::handleStoreMiss(uint32_t memoryAddress){
-    // What to do if it misses for the diffent cases of write-allocate or no-write-allocate and write-through or write-back
-    if(writeAllocate){
-        // Handle either write through or write back case
-        if (writeThrough){
-            load(memoryAddress, false);
+void CacheSimulator::handleLoadMiss(Set& set, uint32_t tag, bool isDirty){ //tag for index not found, get data from memory and load into cache
+    Slot &chosenSlot = set.slots.at(chooseSlotIndex(set));
 
-            // handleCycles
-            totCycles += 100; 
-        } else {
-            load(memoryAddress, true);
+    if(!evictionLRU){
+        chosenSlot.load_ts = ++timestamp;
+    }
+
+    if(chosenSlot.valid && chosenSlot.dirty){ // add 100 cycles per 4 byte quantity
+        totCycles += ((blockSize / 4)  * 100);
+    }
+
+    chosenSlot.tag = tag;
+    chosenSlot.valid = true;
+    chosenSlot.dirty = isDirty;
+    chosenSlot.access_ts = ++timestamp;
+    totCycles += ((blockSize / 4) * 100); //add 100 cycles per 4 byte quantity
+}
+
+bool CacheSimulator::store(uint32_t memoryAddress){ //writing to cache
+    uint32_t tag = readTag(memoryAddress);
+    uint32_t index = readIndex(memoryAddress);
+    Set &set = cache.sets[index];
+    
+    // Check if the tag is already in the cache (cache hit)
+    for (unsigned int i = 0; i < set.slots.size(); i++){
+        Slot &slot = set.slots.at(i);  // Use a reference to modify the slot directly
+        if(slot.valid && slot.tag == tag){
+            timestamp++;
+            slot.access_ts = timestamp;
+
+            // Perform the appropriate action based on writeThrough parameter
+            if (writeThrough) {
+                totCycles += 100; // Handle cycles
+                slot.dirty = false;
+            } else {
+                slot.dirty = true; // Mark the slot as dirty since we're storing data
+            } 
+            
+            totCycles++; // Increment total cycles for the operation
+            return true;
         }
+    }
+    
+    handleStoreMiss(memoryAddress); // Handle a store miss
+    return false; // Return false if the store was unsuccessful
+}
+
+
+void CacheSimulator::handleStoreMiss(uint32_t memoryAddress){
+    // On a store miss, load the relevant block into the cache if write-allocate
+    if (writeAllocate) {
+        load(memoryAddress, true); // Load and mark as dirty
     } else {
-        // handleCycles
-        totCycles+=100; 
+        // No-write-allocate: Write directly to memory without cache modification
+        totCycles += 100; // Writing to memory directly (100 cycles)
     }
 }
 
