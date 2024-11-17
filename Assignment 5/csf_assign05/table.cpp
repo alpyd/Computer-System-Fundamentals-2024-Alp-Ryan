d61d7dd21cfb2b96a395312f1095e02a9f96b873
alpyd <aedemirtas04@gmail.com>
2024-11-17 00:42:48 -0500

    Wrote table and ValueStack

--- Assignment 5/csf_assign05/table.cpp
+++ Assignment 5/csf_assign05/table.cpp
@@ -5,52 +5,71 @@
 
 Table::Table( const std::string &name )
   : m_name( name )
-  // TODO: initialize additional member variables
 {
-  // TODO: implement
 }
 
 Table::~Table()
 {
-  // TODO: implement
 }
 
 void Table::lock()
 {
-  // TODO: implement
+  table_mutex.lock();
 }
 
 void Table::unlock()
 {
-  // TODO: implement
+  table_mutex.unlock();
 }
 
 bool Table::trylock()
 {
-  // TODO: implement
+  return table_mutex.try_lock();
 }
 
 void Table::set( const std::string &key, const std::string &value )
 {
-  // TODO: implement
+  if (committed_data.find(key) != committed_data.end()) {
+    if (committed_data.at(key) != value) {
+      has_tentative_changes = true;
+    }
+  }
+  tentative_data[key] = value;
+
 }
 
 std::string Table::get( const std::string &key )
 {
-  // TODO: implement
+  for (const auto& tuple: tentative_data) {
+    if (tuple.first == key) {
+      return tuple.second;
+    }
+  }
+
+  for (const auto& tuple: committed_data) {
+    if (tuple.first == key) {
+      return tuple.second;
+    }
+  }
+
+  throw std::out_of_range("Element could not be found");
 }
 
 bool Table::has_key( const std::string &key )
 {
-  // TODO: implement
+  bool tentative_has_key = tentative_data.find(key) != tentative_data.end();
+  bool committed_has_key = committed_data.find(key) != committed_data.end();
+  return tentative_has_key || committed_has_key;
 }
 
 void Table::commit_changes()
 {
-  // TODO: implement
+  committed_data = tentative_data;
+  has_tentative_changes = false;
 }
 
 void Table::rollback_changes()
 {
-  // TODO: implement
+  tentative_data = committed_data;
+  has_tentative_changes = false;
 }
